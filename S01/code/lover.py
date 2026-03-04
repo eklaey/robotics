e5f4305571b8e8d2aa5a122703761dc8f998aba8
henrykviana <henrykviana@gmail.com>
2026-03-04 08:54:54 +0100

    Implementation of Explorer & Advanced Lover (untested)

--- S01/code/lover.py
+++ S01/code/lover.py
@@ -1,4 +1,4 @@
-# Basic lover implementation
+# Lover implementation - Braitenberg vehicle with parallel-coupled motors
 from unifr_api_epuck import wrapper
 
 MY_IP = '192.168.2.206'  # change robot number
@@ -7,15 +7,30 @@ robot = wrapper.get_robot(MY_IP)
 NORM_SPEED = 1.5
 PROX_TH = 250
 
+# Weights for weighted proximity calculation - can be tuned for different behaviors (comment/uncomment)
+a, b, c, d = 1, 1, 1, 1        # Equal weights for all sensors
+# a, b, c, d = 2, 1, 1, 0.5     # More weight to front sensors...
+# a, b, c, d = 0.5, 1, 1, 2     # More weight to rear sensors...
+
 robot.init_sensors()
 robot.calibrate_prox()
 
-#infinite loop
+# infinite loop
 while robot.go_on():
     prox_values = robot.get_calibrate_prox()
-    prox = (prox_values[0] + prox_values[7])/2
-    ds = (NORM_SPEED * prox) / PROX_TH
-    speed = NORM_SPEED - ds
-    robot.set_speed(speed)
+    
+    # Calculate weighted average for right and left sensors
+    prox_right = (a * prox_values[0] + b * prox_values[1] + c * prox_values[2] + d * prox_values[3]) / (a + b + c + d)
+    prox_left = (a * prox_values[7] + b * prox_values[6] + c * prox_values[5] + d * prox_values[4]) / (a + b + c + d)
+    
+    # Calculate delta speeds using Braitenberg formula: ds = -(prox/prox_th) * s0
+    ds_left = (NORM_SPEED * prox_left) / PROX_TH    # Left sensor controls left motor (parallel-coupled)
+    ds_right = (NORM_SPEED * prox_right) / PROX_TH  # Right sensor controls right motor (parallel-coupled)
+    
+    # Calculate individual motor speeds: s = s0 + ds
+    left_speed = NORM_SPEED - ds_left
+    right_speed = NORM_SPEED - ds_right
+    
+    robot.set_speed(left_speed, right_speed)
     
 robot.clean_up()
